7a5567852338b5e8ca43deeced66817b2867be67
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-03 08:51:13 -0500

    Plugging in random stuff, not sure what works and what doesnt

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,77 @@
-$(document).ready(onReady);
+$(document).ready(init);
 
-function onReady() {
-    console.log('woof');
+let operation = "";
 
+function init() {
+    $("#js-btn-add").on("click", clickAdd);
+    $("#js-btn-subtract").on("click", clickSubtract);
+    $("#js-btn-multiply").on("click", clickMultiply);
+    $("#js-btn-divide").on("click", clickDivide);
+    $("#js-btn-equals").on("click", clickEquals);
+    $("#js-btn-clear").on("click", clickClear);
+
+    getEquations();
+}
+
+function clickAdd() {
+    operation = "Add";
+}
+
+function clickSubtract() {
+    operation = "Subtract";
+}
+
+function clickMultiply() {
+    operation = "Multiply";
+}
+
+function clickDivide() {
+    operation = "Divide";
+}
+
+function clickEquals() {
+    // WRONG - operation = "Equals";
+
+    const dataForServer = {
+        num1: $("#js-user-input1").val(),
+        num2: $("#js-user-input2").val(),
+        operation: operation
+    };
+    postEquation(dataForServer)
+}
+
+function clickClear() {
+    operation = "Clear";
+}
+
+function postEquation(dataForServer) {
+    $.ajax({
+            type: "POST",
+            url: "/math",
+            data: dataForServer,
+        })
+        .then(response => {
+            console.log(response);
+            getEquations();
+        })
+
+        .catch(err => {
+            console.warn(err);
+        });
+}
+
+function getEquations() {
+    $.ajax({
+            type: "GET",
+            url: "/math",
+        })
+        .then(response => {
+            render(response);
+            console.log(response);
+        })
+        .catch(err => {
+            console.warn(err);
+        });
+
+    function render(response) {}
 }
--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 //set up our server
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const PORT = 5000;
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static('server/public'));
 
 app.get('/meow', (req, res) => {
-    res.send('Meow');
+  res.send('Meow');
 });
 
-app.listen(5000, () => {
-    console.log('Server up and running');
+app.listen(PORT, () => {
+  console.log('Server up and running');
 });
