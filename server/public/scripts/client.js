1098c6fa206e137529ab9f648430deb75c0c9358
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-05 17:06:10 -0500

    Append and empty is working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,87 +4,95 @@ let operation;
 let equation = [];
 
 function init() {
-  $('#js-btn-add').on('click', clickAdd);
-  $('#js-btn-subtract').on('click', clickSubtract);
-  $('#js-btn-multiply').on('click', clickMultiply);
-  $('#js-btn-divide').on('click', clickDivide);
-  $('#js-btn-equals').on('click', clickEquals);
-  $('#js-btn-clear').on('click', clickClear);
+    $('#js-btn-add').on('click', clickAdd);
+    $('#js-btn-subtract').on('click', clickSubtract);
+    $('#js-btn-multiply').on('click', clickMultiply);
+    $('#js-btn-divide').on('click', clickDivide);
+    $('#js-btn-equals').on('click', clickEquals);
+    $('#js-btn-clear').on('click', clickClear);
 
-  getEquations();
+    getEquations();
 }
 
 function clickAdd() {
-  operation = 'add';
-  //   $('#js-btn-add').val();
-  console.log(operation);
+    operation = 'add';
+    //   $('#js-btn-add').val();
+    console.log(operation);
 }
 
 function clickSubtract() {
-  operation = 'subtract';
+    operation = 'subtract';
 }
 
 function clickMultiply() {
-  operation = 'multiply';
+    operation = 'multiply';
 }
 
 function clickDivide() {
-  operation = 'divide';
+    operation = 'divide';
 }
 
 function clickEquals() {
-  //   operation = 'equals';
-  //   console.log('Equaled');
+    //   operation = 'equals';
+    //   console.log('Equaled');
 
-  const dataForServer = {
-    num1: parseInt($('#input-field-1').val()),
-    num2: parseInt($('#input-field-2').val()),
-    operation: operation,
-  };
-  console.log(dataForServer);
-  postEquation(dataForServer);
+    const dataForServer = {
+        num1: parseInt($('#input-field-1').val()),
+        num2: parseInt($('#input-field-2').val()),
+        operation: operation,
+    };
+    console.log(dataForServer);
+    postEquation(dataForServer);
 }
 
 function clickClear() {
-  operation = 'clear';
+    operation = 'clear';
 }
 
 function postEquation(dataForServer) {
-  $.ajax({
-    type: 'POST',
-    url: '/math',
-    data: dataForServer,
-  })
-    .then((response) => {
-      console.log('POST', response);
-      getEquations();
-    })
+    $.ajax({
+            type: 'POST',
+            url: '/math',
+            data: dataForServer,
+        })
+        .then((response) => {
+            console.log('POST', response);
+            getEquations();
+        })
 
-    .catch((err) => {
-      console.warn(err);
-    });
+        .catch((err) => {
+            console.warn(err);
+        });
 }
 
 function getEquations() {
-  $.ajax({
-    type: 'GET',
-    url: '/math',
-  })
-    .then((response) => {
-      render(response);
-      console.log(response);
-    })
-    .catch((err) => {
-      console.warn(err);
-    });
+    $.ajax({
+            type: 'GET',
+            url: '/math',
+        })
+        .then((response) => {
+            render(response);
+            console.log(response);
+        })
+        .catch((err) => {
+            console.warn(err);
+        });
 }
 
+// function render(answerObject) {
+//     console.log(answerObject);
+//     $('.js-output').append(
+//         `<h2>${answerObject[answerObject.length - 1].answer} </h2>`
+//     );
+//     $('.js-output').empty();
 function render(answerObject) {
-  console.log(answerObject);
-  for (let equation of answerObject) {
-    $('#js-output').append(`<h2>${equation.answer} </h2>`);
-  }
-  //   $('#js-output').empty();
+    $('#js-output').empty();
+    // console.log(answerObject);
+    for (let equation of answerObject) {
+        $('#js-output').append(`<h4>${equation.answer} </h4>`);
+
+        // $('#js-output').empty(`<h2>${equation.answer} </h2>`);
+    }
 }
 //GET answer from POST(server) to client using get route
 //create variable globally
