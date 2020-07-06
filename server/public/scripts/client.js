07b2e69d5af5e270a7b9eb6f13cf843e63c33034
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-06 10:56:54 -0500

    Updating append for all operations

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,81 +5,81 @@ let equation = [];
 //Defining operation and setting equation (req.body) to empty array.
 
 function init() {
-    $('#js-btn-add').on('click', clickAdd);
-    $('#js-btn-subtract').on('click', clickSubtract);
-    $('#js-btn-multiply').on('click', clickMultiply);
-    $('#js-btn-divide').on('click', clickDivide);
-    $('#js-btn-equals').on('click', clickEquals);
-    $('#js-btn-clear').on('click', clickClear);
+  $('#js-btn-add').on('click', clickAdd);
+  $('#js-btn-subtract').on('click', clickSubtract);
+  $('#js-btn-multiply').on('click', clickMultiply);
+  $('#js-btn-divide').on('click', clickDivide);
+  $('#js-btn-equals').on('click', clickEquals);
+  $('#js-btn-clear').on('click', clickClear);
 
-    getEquations();
+  getEquations();
 }
 //init function that has our click events for each type of operation.
 
 function clickAdd() {
-    operation = 'add';
-    //   $('#js-btn-add').val();
-    console.log(operation);
+  operation = 'add';
+  //   $('#js-btn-add').val();
+  console.log(operation);
 }
 
 function clickSubtract() {
-    operation = 'subtract';
+  operation = 'subtract';
 }
 
 function clickMultiply() {
-    operation = 'multiply';
+  operation = 'multiply';
 }
 
 function clickDivide() {
-    operation = 'divide';
+  operation = 'divide';
 }
 
 function clickEquals() {
-    //   operation = 'equals';
-    //   console.log('Equaled');
-    //functions for each type of operation (+ - * /)
-    const dataForServer = {
-        num1: parseInt($('#input-field-1').val()),
-        num2: parseInt($('#input-field-2').val()),
-        operation: operation,
-    };
-    console.log(dataForServer);
-    postEquation(dataForServer);
+  //   operation = 'equals';
+  //   console.log('Equaled');
+  //functions for each type of operation (+ - * /)
+  const dataForServer = {
+    num1: parseInt($('#input-field-1').val()),
+    num2: parseInt($('#input-field-2').val()),
+    operation: operation,
+  };
+  console.log(dataForServer);
+  postEquation(dataForServer);
 }
 //turning our numbers from strings to numbers using parseInt
 
 function clickClear() {
-    operation = 'clear';
+  operation = 'clear';
 }
 
 function postEquation(dataForServer) {
-    $.ajax({
-            type: 'POST',
-            url: '/math',
-            data: dataForServer,
-        })
-        .then((response) => {
-            console.log('POST', response);
-            getEquations();
-        })
+  $.ajax({
+    type: 'POST',
+    url: '/math',
+    data: dataForServer,
+  })
+    .then((response) => {
+      console.log('POST', response);
+      getEquations();
+    })
 
-        .catch((err) => {
-            console.warn(err);
-        });
+    .catch((err) => {
+      console.warn(err);
+    });
 } //post route to send dataFromServer which is our re.body/ equation
 
 function getEquations() {
-    $.ajax({
-            type: 'GET',
-            url: '/math',
-        })
-        .then((response) => {
-            render(response);
-            console.log(response);
-        })
-        .catch((err) => {
-            console.warn(err);
-        });
+  $.ajax({
+    type: 'GET',
+    url: '/math',
+  })
+    .then((response) => {
+      render(response);
+      console.log(response);
+    })
+    .catch((err) => {
+      console.warn(err);
+    });
 } // GET route to get our response (not positive how to explain it? maybe idk it well enough.)
 
 // function render(answerObject) {
@@ -88,18 +88,19 @@ function getEquations() {
 //         `<h2>${answerObject[answerObject.length - 1].answer} </h2>`
 //     );
 //     $('.js-output').empty();
-function render(answerObject) {
-    $('#js-output').empty();
-    // console.log(answerObject);
-    for (let equation of answerObject) {
-        $('#js-output').append(`<h4>${equation.answer} </h4>`);
-
-        // $('#js-output').empty(`<h2>${equation.answer} </h2>`);
+function render(answerList) {
+  $('#js-output').empty();
+  // console.log(answerObject);
+  for (let equation of answerList) {
+    $('#js-output').append(`<h4>${equation.answer} </h4>`);
+    for (let equation of answerList) {
+      $('#js-all-output').append(
+        `<li>${equation.num1} ${equation.operation} ${equation.num2} ${equation.operation}<li>`
+      );
     }
+    // $('#js-output').empty(`<h2>${equation.answer} </h2>`);
+  }
 } // render function the appends and empties our equations
-
-
-
 
 //GET answer from POST(server) to client using get route
 //create variable globally
